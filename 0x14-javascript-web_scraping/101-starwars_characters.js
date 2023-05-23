0020407edac00930c7cbe21f3c89075461937dbf
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-23 11:22:23 +0200

    Update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,41 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const filmId = process.argv[2];
+const filmUrl = `https://swapi.dev/api/films/${filmId}/`;
+
+request(filmUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    fetchCharacters(characters);
+  } else {
+    console.error('Error:', error);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+function fetchCharacters(characters) {
+  let counter = 0;
+
+  function fetchCharacter(url) {
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(body);
+        console.log(characterData.name);
+
+        counter++;
+        if (counter < characters.length) {
+          fetchCharacter(characters[counter]);
+        }
+      } else {
+        console.error('Error:', error);
       }
-    }
-  });
+    });
+  }
+
+  if (characters.length > 0) {
+    fetchCharacter(characters[counter]);
+  }
 }
