d38daf877c8a024db5d290387e932f1c3ea68457
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-23 11:08:13 +0200

    Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi.dev/api/people/';
+
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+  if (!error && response.statusCode === 200) {
+    const results = JSON.parse(body).results;
+    results.forEach((character) => {
+      console.log(character.name);
     });
+  } else {
+    console.error('Error:', error);
   }
 });
