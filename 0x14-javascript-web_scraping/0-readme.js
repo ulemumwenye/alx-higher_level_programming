f9102856f966d0211f1602cb4869448b5f60c88b
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-24 13:20:40 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
+const process = require('process');
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+// The first argument is the file path
+const file = process.argv[2];
+// The content of the file must be written in utf-8
+fs.readFile(file, 'utf8', function (err, data) {
+  // If an error occurred during the reading, print the error object
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
